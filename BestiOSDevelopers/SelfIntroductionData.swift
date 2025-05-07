6f8020efa08f1717db8622302926621ce5e077ae
Hyunjun Park <142282197+morgan4563@users.noreply.github.com>
2025-05-07 17:41:22 +0900

    최정은 메니저님 자기 소개 수정

--- BestiOSDevelopers/SelfIntroductionData.swift
+++ BestiOSDevelopers/SelfIntroductionData.swift
@@ -2,9 +2,9 @@ let people: [SelfIntroduction] = [
     SelfIntroduction(
         name: "최정은",
         trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
+        hobby: "사람만나기",
+        mbti: "INSJ",
+        strength: "활발하고 밝다",
         sns: nil,
         etc: "열심히 수료하고 취업하겠습니다!",
         job: "네이버 - iOS 개발자"
