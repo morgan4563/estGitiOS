954890421b57743743aca72aca6c4e6f0c56085f
전광호 <haksang1002@gmail.com>
2025-05-08 15:55:02 +0900

    첫번째 커밋

--- BestiOSDevelopers/SelfIntroductionData.swift
+++ BestiOSDevelopers/SelfIntroductionData.swift
@@ -362,8 +362,8 @@ let people: [SelfIntroduction] = [
     SelfIntroduction(
         name: "전광호",
         trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
+        hobby: "음악 감상, 영화 시청",
+        mbti: "ISFP",
         strength: "끈기와 실행력",
         sns: nil,
         etc: "열심히 수료하고 취업하겠습니다!",
