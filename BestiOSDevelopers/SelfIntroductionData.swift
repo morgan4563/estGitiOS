9ef41f25bfb3319e83fd14b544d166be03801752
morgan4563 <morganpark4563@gmail.com>
2025-05-09 08:45:29 +0900

    강사진, 팀원 외 내용 삭제

--- BestiOSDevelopers/SelfIntroductionData.swift
+++ BestiOSDevelopers/SelfIntroductionData.swift
@@ -10,6 +10,16 @@ let people: [SelfIntroduction] = [
         job: "네이버 - iOS 개발자"
     ),
     SelfIntroduction(
+        name: "계주성",
+        trainingPurpose: "iOS 강사가 되기위해 참여",
+        hobby: "글쓰기, 독서",
+        mbti: "ETNP",
+        strength: "피스메이커",
+        sns: "http.v.v.v",
+        etc: "열심히 수료하고 취업하겠습니다!",
+        job: "삼성 - iOS 개발자"
+    ),
+    SelfIntroduction(
         name: "김근영",
         trainingPurpose: "iOS 강의를 위해",
         hobby: "치킨 먹기",
@@ -18,146 +28,6 @@ let people: [SelfIntroduction] = [
         sns: nil,
         etc: "100퍼센트 취업할 수 있도록 돕겠습니다",
         job: "iOS 강사"
-    ),
-    SelfIntroduction(
-        name: "이준석",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "네이버 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "최진원",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "삼성 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "윤혜주",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "마이크로소프트 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "이유정",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "SK - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "서재효",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "네이버 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "김민창",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "토스 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "강대훈",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "네이버 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "서희재",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "LG - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "고민혁",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "네이버 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "이강호",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "배달의민족 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "백현진",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "네이버 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "이준영",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "구글 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "홍승아",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "쿠팡 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "권도현",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "삼성 - iOS 개발자"
     ),
     SelfIntroduction(
         name: "김유진",
@@ -170,26 +40,6 @@ let people: [SelfIntroduction] = [
         job: "쿠팡 - iOS 개발자"
     ),
     SelfIntroduction(
-        name: "성주현",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "메타 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "김건우",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "구글 - iOS 개발자"
-    ),
-    SelfIntroduction(
         name: "김동녕",
         trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
         hobby: "음악 감상, 여행, 독서 등",
@@ -200,49 +50,9 @@ let people: [SelfIntroduction] = [
         job: "마이크로소프트 - iOS 개발자"
     ),
     SelfIntroduction(
-        name: "정지훈",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "마이크로소프트 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "노기승",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "구글 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "김현식",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "마이크로소프트 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "김경언",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "토스 - iOS 개발자"
-    ),
-    SelfIntroduction(
         name: "박현준",
         trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여함",
-        hobby: "유튜브 보기, 티비보기222",
+        hobby: "유튜브 보기, 티비보기",
         mbti: "ENTJ",
         strength: "끈기와 실행력, 헤헤",
         sns: nil,
@@ -250,205 +60,13 @@ let people: [SelfIntroduction] = [
         job: "네이버 - iOS 개발자"
     ),
     SelfIntroduction(
-        name: "전윤철",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "배달의민족 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "이승민",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "네이버 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "김효환",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "쿠팡 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "박동언",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "쿠팡 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "이동욱",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "카카오 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "김재훈",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "배달의민족 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "김서현",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "마이크로소프트 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "김종성",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "배달의민족 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "이주용",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "삼성 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "이준일",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "테슬라 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "양희정",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "마이크로소프트 - iOS 개발자"
-    ),
-    SelfIntroduction(
         name: "전광호",
         trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
         hobby: "음악 감상, 독서",
         mbti: "ISFP",
-        // 주석추가 1
         strength: "끈기와 실행력, 끈기끈기",
         sns: nil,
-        // 주석추가2
         etc: "열심히 수료하고 취업하겠습니다!, 화이팅",
         job: "마이크로소프트 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "고재현",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "구글 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "계주성",
-        trainingPurpose: "iOS 강사가 되기위해 참여",
-        hobby: "글쓰기, 독서",
-        mbti: "ETNP",
-        strength: "피스메이커",
-        sns: "http.v.v.v",
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "삼성 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "강민지",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "삼성 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "장지현",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "배달의민족 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "하재준",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "SK - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "정소이",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "구글 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "이치훈",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "삼성 - iOS 개발자"
-    ),
-    SelfIntroduction(
-        name: "신유섭",
-        trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
-        mbti: "INTJ",
-        strength: "끈기와 실행력",
-        sns: nil,
-        etc: "열심히 수료하고 취업하겠습니다!",
-        job: "토스 - iOS 개발자"
-    ),
+    )
 ]
